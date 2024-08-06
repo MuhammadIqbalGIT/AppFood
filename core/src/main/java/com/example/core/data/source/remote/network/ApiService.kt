39ad290cb9de03102c9f25a072350aa25f1991f6
Muhammad iqbal <iqbalvzr@gmail.com>
2024-08-06 22:43:12 +0700

    Initial commit

---
+++ core/src/main/java/com/example/core/data/source/remote/network/ApiService.kt
@@ -0,0 +1,28 @@
+package com.example.core.data.source.remote.network
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+   /* @GET("movie/now_playing")
+    suspend fun getPopularMovies(
+        @Query("api_key") apiKey: String,
+        @Query("page") page: Int
+    ) : Response<MovieListResponse>
+
+
+    @GET("movie/{movie_id}/credits")
+    suspend fun getCreditsMovie(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String
+    ) : Response<CreditsListResponse>
+
+    @GET("movie/{movie_id}/similar")
+    suspend fun getSimiliarMovie(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String
+    ) : Response<MovieListResponse>*/
+
+}
---
+++ core/src/main/java/com/example/core/di/CoreModule.kt
@@ -0,0 +1,36 @@
+package com.example.core.di
+
+import com.example.core.data.source.remote.network.ApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val networkModule = module {
+
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+
+    /*single {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(Constant.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+        retrofit.create(ApiService::class.java)
+    }*/
+}
+
+/*
+val repositoryModule = module {
+    single<IMovieRepository> { MovieRepository(get()) }
+    single { DetailDataSource(get()) }
+    single<IMovieDetailRepository> { MovieDetailRepository(get()) }
+}*/
